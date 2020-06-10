8b8fa790e19fabaa7d00ad2a1ae56072fd3c9e45
Mahmoud Mansour <mahmodlte@gmail.com>
2020-06-10 09:23:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code here
+function saturdayFun (thing="roller-skate"){
+  return `This Saturday, I want to ${thing}!`
+}
+ let mondayWork = function(work='go to the office'){
+   return `This Monday, I will ${work}`
+ }
+ mondayWork("Play games")
