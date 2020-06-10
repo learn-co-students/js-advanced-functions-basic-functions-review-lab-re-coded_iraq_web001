8d136486751292057197018f6a737d0e70a7abd2
Mahmoud Mansour <mahmodlte@gmail.com>
2020-06-10 12:26:29 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+function saturdayFun (thing='roller-skate'){
+    return `This Saturday, I want to ${thing}!`
+}
+
+let mondayWork =function (work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+mondayWork();
+
+
+ let wrapAdjective =  function (flair =`*`){
+
+
+    return function(any =" special") {
+        return `You are ${flair}${any}${flair}!`
+    }
+
+}
+
+let Calculator = {
+};
+
+Calculator.add = function(n1=1, n2=3){
+    return n1 + n2
+}
+Calculator.subtract = function(n1 = 1, n2 =3){
+    return n1 - n2
+}
+Calculator.multiply = function(n1 = 1 , n2 = 3){
+    return n1 * n2
+}
+Calculator.divide = function(n1= 10, n2 = 5){
+    return n1 / n2
+}
+
+function actionApplyer (num, arr){
+    if (arr.length === 0){
+        return num;
+    }else {
+
+        return (((num * 2) + 1000) % 7) ;
+    }
+    
+
+
+}
